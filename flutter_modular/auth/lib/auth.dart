2a91bbe2bcd3816808fcc0aa4921051d7b0f0877
David Samuel <david.sisnet@gmail.com>
2023-07-12 23:01:03 -0400

    started implementation of the login screen

--- flutter_modular/auth/lib/auth.dart
+++ flutter_modular/auth/lib/auth.dart
@@ -8,8 +8,51 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+        child: SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextField(
+          onChanged: (String text) {
+            email = text;
+          },
+          keyboardType: TextInputType.emailAddress,
+          decoration: const InputDecoration(
+            labelText: 'EMAIL',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextField(
+          onChanged: (String text) {
+            password = text;
+          },
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: 'PASSWORD',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (email == 'david@mpto.gov.br' && password == '123') {
+              print('');
+            }
+          },
+          child: const Text('Enter'),
+        )
+      ]),
+    ));
   }
 }
